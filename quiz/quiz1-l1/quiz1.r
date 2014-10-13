a6e02a7ee03d5d94d94337d921466a0b78bb88ef
nicholasrogers <nicholasrogers27@berkeley.edu>
2014-10-13 09:47:36 -0700

    Done

--- quiz/quiz1-l1/quiz1.r
+++ quiz/quiz1-l1/quiz1.r
@@ -1,14 +1,14 @@
 # Please load in the dataset included in the quiz1-l1 directory. It will be
 # required to perform the following tasks. The dataset includes data for houses
 # in the city of Alameda.
-
+load("SFhousing-1.rda")
 
 
 # calculate the mean and median price of houses in Alameda. Store these as the
 # variables <mean.price> and <med.price> respectively.
 
-# mean.price <- your code here
-# med.price <- your code here
+mean.price <- mean(housing$price)
+med.price <- median(housing$price)
 
 
 
@@ -17,8 +17,7 @@
 # <price.diffs>. Note that this should be a numeric vector with length equal to
 # the number of observations in the dataset
 
-# price.diffs <- your code here
-
+price.diffs <- housing$price - mean.price
 
 # The variable br indicates the number of bedrooms in each house. Please create
 # two new data frames that are subsets of the original data frame, according to
@@ -26,8 +25,8 @@
 # 1) houses with more than 3 rooms <housing.large>
 # 2) houses with up to and including 3 rooms  <housing.small>
 
-# housing.large <- your code here
-# housing.small <- your code here
+housing.large <- subset(housing, housing$br > 3)
+housing.small <- subset(housing, housing$br <= 3)
 
 
 
@@ -35,8 +34,8 @@
 # For each of your subsets, create a vector giving the price of each house. Name
 # these variables <housing.large.price> and <housing.small.price>.
 
-# housing.large.price <- your code here
-# housing.small.price <- your code here
+housing.large.price <- housing.large$price 
+housing.small.price <- housing.small$price
 
 
 
@@ -52,8 +51,13 @@
 # Your function should return the average of <lsqft> for all observations with <price>
 # stricly greater than <price.cutoff>.
 sqftByPrice <- function(price.cutoff, prices, lsqft) {
-
-    # your code here
+         price.greater <- subset(prices, prices > price.cutoff, na.rm = TRUE) 
+         housing.asubset <- subset(housing, housing$price %in% price.greater, na.rm = TRUE)
+         housing.subset.lsqft <- housing.asubset$lsqft
+         housing.lsqft.subset <- subset(housing.subset.lsqft, housing.subset.lsqft %in% lsqft, na.rm = TRUE)
+         lsqft.average <- mean(housing.lsqft.subset, na.rm = TRUE)
+         return(lsqft.average)
+    
 
 }
 
@@ -64,3 +68,5 @@ sqftByPrice <- function(price.cutoff, prices, lsqft) {
 # 2) a red line with intercept=169500 and slope=275
 # 3) plotting character set to 20
 
+plot(housing$bsqft, housing$price, main = "Housing price vs Building sqft", pch = 20)
+abline(a = 169500, b = 275, col = "Red")
